2a72ed1b177dfbffbb761c53f5ad5957a4735df4
Kasia <kasia@localhost.localdomain>
2018-03-29 12:31:54 +0200

    working program 1

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import numpy as np
 
 if __name__ == "__main__":
-    print(np.abs(-7))
-    print("aaa")
+    # TODO: ZROBIC TAKI SAM PROGRAM BEZ UZYCIA DWOCH LIst (imiona i nazwiska) - uzyc jednej z krotkami - tuple - (a konkretnie parami)
+    imiona = []
+    nazwiska = []
+
+    taknie = "tak"
+    while taknie == "tak":
+        print("podaj imie")
+        imie = input()
+        imiona.append(imie)
+        print("podaj nazwisko")
+        nazwisko = input()
+        nazwiska.append(nazwisko)
+        print("czy chcesz dodac kolejnego ucznia?")
+        taknie = input()
+    for index in range(0, len(imiona)):
+        print("imie: " + imiona[index] + ", nazwisko: " + nazwiska[index])
