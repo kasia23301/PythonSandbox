a4a4ceb6a7f2419cd3e54af60a4fb708b61ec660
Kasia <kasia@localhost.localdomain>
2018-07-12 16:06:58 +0200

    Pracuj a nie sie opie

---
+++ RuszGlowa/vowels2.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    vowels = ["a", "e", "i", "o", "u"]
+    word= input("Podaj słowo, w którym należy wyszuakć samogłoski: ")
+    found = []
+    for letter in word:
+        if letter in vowels:
+            if letter not in found:
+                found.append(letter)
+    for vowel in found:
+        print(vowel)
