51e7a444ba43b3196092e9705fcf2716fcdb14b9
Kasia <kasia@localhost.localdomain>
2018-08-07 16:57:20 +0200

    my message

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,19 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
 
-@app.route('/')
-def hello() -> str:
-    return 'Witaj, świecie, tu Flask!'
 
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search4letters('życie, wszechświat i cała reszta', 'eiru,!'))
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Oto Twoje wyniki: '
+    results = str(search4letters(phrase.upper(), letters.upper()))
+    return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results)
+
+
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Witamy na stronie internetowej search4letters!')
+
 
 app.run(debug=True)
