cf6fc153268767c1b5ae3dbe8f4e0b7545e7a6a9
kasia233 <33174189+kasia233@users.noreply.github.com>
2018-08-20 20:27:03 +0200

    nie dzialaxd

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+import mysql.connector
 
 app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(str(dir(req)), res, file=log)
+    dbconfig = {'host': '127.0.0.1', 'user': 'vsearch', 'password': 'vsearchpasswd', 'database': 'vsearchlogDB'}
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into log (phrase, letters, ip, browser_string, results)
+    values (%s,%s,%s,%s,%s)"""
+    cursor.execute(_SQL, ("galaktyka", "tym", "127.0.0.1", "Opera", "{'t', 'y'}"))
+    conn.commit()
+    _SQL = '''select * from log'''
+    cursor.execute(_SQL)
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    conn.close()
+
 
 
 
