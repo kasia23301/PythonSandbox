17760350337862c8d347f15a2c8b1a9063b90620
mattopsh <langopsh@gmail.com>
2018-08-24 22:33:19 +0200

    parking table

---
+++ parking/populate.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+if __name__ == "__main__":
+    '''
+    create table parking(
+       id int auto_increment primary key,
+       name varchar(255) not null,
+       attitude float not null,
+       longitude float not null,
+       free_spaces int not null,
+       capacity int not null,
+       rating int
+    )
+    '''
+
+
