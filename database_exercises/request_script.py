46949ce4cebdfecf135151e7f5c4ca0574a791f0
kasia233 <33174189+kasia233@users.noreply.github.com>
2019-01-15 12:07:45 +0100

    changes

--- database_exercises/request_script.py
+++ database_exercises/request_script.py
@@ -1,4 +1,22 @@
 import requests
+import mysql.connector
 
 if __name__ == "__main__":
     r = requests.get('https://parking-finder-spring.herokuapp.com/api/v1/parking/public/nearToLocation')
+    dbconfig = {'host': '127.0.0.1',
+                'user': 'root',
+                'password': 'arogontaldo',
+                'database': 'Parking_finder'}
+    conn = mysql.connector.connect(**dbconfig)
+    parkingName = r["parkingName"]
+    parkingAddress = r["parkingAddress"]
+    attitude = r["attitude"]
+    longitude = r["longitude"]
+    freeSpaces = r["freeSpaces"]
+    capacity = r["capacity"]
+    rating = r["rating"]
+    _SQL = "insert into parking values(NULL,'" + str(parkingName) + "'," + str(parkingAddress) + "," + str(
+        attitude) + "," + str(longitude) + "," + str(freeSpaces) + "," + str(capacity) + ","
+    str(rating) + ")"
+    cursor = conn.cursor()
+    cursor.execute(_SQL)
