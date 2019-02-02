d7a6dde330568747a65e9956c7b0550f36b2062f
kasia233 <33174189+kasia233@users.noreply.github.com>
2019-02-02 20:30:32 +0100

    csv

--- database_exercises/save_to_csv_file.py
+++ database_exercises/save_to_csv_file.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import csv
 
 if __name__ == "__main__":
     dbconfig = {'host': '127.0.0.1',
@@ -11,9 +12,9 @@ if __name__ == "__main__":
     cursor.execute(
         "SELECT parkingName, parkingAddress, attitude, longitude, distance, freeSpaces, dataVeracity FROM Parking ")
     result_set = cursor.fetchall()
-    with open('datafile.txt', 'a') as file:
+    with open("backup.csv", mode="w") as backup:
+        backup_writer = csv.writer(backup, delimiter=',')
         for row in result_set:
-            file.write("%s, %s, %s, %s, %s, %s, %s\n" % (
+            backup_writer.writerow("%s, %s, %s, %s, %s, %s, %s\n" % (
                 row["parkingName"], row["parkingAddress"], row["attitude"], row["longitude"], row["distance"],
                 row["freeSpaces"], row["dataVeracity"]))
-
