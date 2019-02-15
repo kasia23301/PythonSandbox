d48adf76d454bcd533e8a2863a834294fc3ce01e
kasia233 <33174189+kasia233@users.noreply.github.com>
2019-02-15 20:02:15 +0100

    cnsjcnscn

--- database_exercises/save_to_database.py
+++ database_exercises/save_to_database.py
@@ -6,12 +6,13 @@ if __name__ == "__main__":
                 'user': 'root',
                 'password': 'arogontaldo',
                 'database': 'parkingfinder'}
+    with open('backup.csv') as File:
+        csv_data = csv.reader(File)
+        conn = mysql.connector.connect(**dbconfig)
+        cursor = conn.cursor()
+        for row in csv_data:
+            sql = "Insert into Parking Values([row[0], row[1], row[2], row[3], row[4],row[5], row[6])"
+            cursor.execute()
+            conn.commit()
+            cursor.close()
 
-    csv_data = csv.reader(file('backup.csv'))
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    for row in csv_data:
-        sql = """'INSERT INTO Parking (parkingName, parkingAddress, attitude, longitude, distance, freeSpaces, dataVeracity) VALUES (""" + row + """)'"""
-        cursor.execute()
-        conn.commit()
-        cursor.close()
